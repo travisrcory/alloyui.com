d3c3d3ea3ccd64e2fda9fdb8cb2542e1d41629b0
TRAVIS_CORY <travis.cory@liferay.com>
2013-07-09 12:36:59 -0700

    Changed color of "Try Me" button in tutorial-demo (".btn-primary" in the bootstrap.css line 3183). Added Carousel to the tutorial demo (took away tool-tip). Changed width of .span9 in main.css.

--- src/files/website/js/tutorial-demo.js
+++ src/files/website/js/tutorial-demo.js
@@ -1,12 +1,14 @@
-AUI().use('node', 'transition', 'aui-tooltip', function (A) {
+AUI().use('node', 'transition', 'aui-carousel', function (A) {
 
   A.one('.btn').on('click', function() {
     this.transition({ width: '400px' });
   });
 
-  var tooltip = new A.Tooltip({
-    trigger: '#tooltip',
-    bodyContent: '<iframe width="500" height="280" src="http://www.youtube.com/embed/KzorZ1CO6Vs" frameborder="0" allowfullscreen></iframe>'
+  new A.Carousel({
+    contentBox: '#myCarousel',
+    intervalTime: 2,
+    width: 700,
+    height: 250
   }).render();
 
 });
