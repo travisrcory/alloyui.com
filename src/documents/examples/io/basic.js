5347d7607c98c0d86e848e4faf4b74dcec696736
TRAVIS_CORY <travis.cory@liferay.com>
2013-07-09 14:35:38 -0700

    Add (io/data) to line 5. This caused the example to display properly.

--- src/documents/examples/io/basic.js
+++ src/documents/examples/io/basic.js
@@ -1,16 +1,16 @@
 AUI().use(
-  'aui-io-request',
-  function (A) {
-    A.io.request(
-      'data/content.html',
-      {
-        on: {
-          success: function() {
-            var data = this.get('responseData');
-            alert(data);
-          }
-        }
-      }
-    );
-  }
+	'aui-io-request',
+	function (A) {
+		A.io.request(
+			'io/data/content.html',
+			{
+				on: {
+					success: function() {
+						var data = this.get('responseData');
+						alert(data);
+					}
+				}
+			}
+		);
+	}
 );
